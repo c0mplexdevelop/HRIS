339595efa171c1a743eeee56a38b8a0c15ad5e98
c0mplexdev <105768978+c0mplexdevelop@users.noreply.github.com>
2024-02-25 23:20:44 +0800

    Changed submit to OnClic, injected Dependencies on codebehind

--- Components/Pages/Login.razor.cs
+++ Components/Pages/Login.razor.cs
@@ -1,9 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Logging;
+using HRIS.Data;
 
 namespace HRIS.Components.Pages
 {
     public partial class Login
     {
+
+        [Inject]
+        private ILogger<Login> Logger { get; set; }
+
+        [Inject]
+        private ApplicationDbContext DbContext { get; set; }
+
         public class UserInput
         {
             [Required]
@@ -17,6 +27,25 @@ namespace HRIS.Components.Pages
         public void Submit()
         {
             Logger.LogInformation($"{model!.UserPassword}");
+            var segments = model.UserPassword!.Split(":");
+            var accessCode = new AccessCode
+            {
+                DateCreated = segments[0],
+                HandlerInitials = segments[1],
+                TransactionCode = segments[2]
+            };
+
+            Console.WriteLine(accessCode.DateCreated);
+
+
+            // q: query the database for the keyless accesscode
+            var codeFromDB = DbContext.AccessCodes.Where(e => e.DateCreated == accessCode.DateCreated).AsEnumerable().ToList();
+
+            // print the codes from codeFromDB
+            Console.WriteLine("Access Codes from DB");
+            Console.WriteLine(codeFromDB[0]);
+
+
         }
     }
 }
