f41f8a52880fb6087e0d609d6414c75c02ae7f04
c0mplexdevelop <105768978+c0mplexdevelop@users.noreply.github.com>
2024-04-27 15:08:25 +0800

    Addeddata annotation for verification later

--- Components/Layout/LoginLayout.razor.cs
+++ Components/Layout/LoginLayout.razor.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HRIS.Components.Layout;
 
 public partial class LoginLayout {
     public class UserInput {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-        public required int AccessCode { get; set; }
+        [Required(ErrorMessage = "Username is required!")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "Username must be between 7 and 50 characters!")]
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string AccessCode { get; set; }
     }
 
     public required UserInput Model { get; set; }
